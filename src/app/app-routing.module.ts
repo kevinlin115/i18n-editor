e711e843758ac04d5461165eb22d3e9e184fa335
kevinlin115 <nash4thewin@gmail.com>
2021-09-09 23:25:15 +0800

    新增兩個假的 a, b component，放到 github page 可以 work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { AComponent } from './components/a/a.component';
+import { BComponent } from './components/b/b.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'a', component: AComponent },
+  { path: 'b', component: BComponent },
+  { path: '**', redirectTo: 'a' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
